570c3fe4daeb778e342093cf915636446eec9fe7
Rohitkr711 <105406217+Rohitkr711@users.noreply.github.com>
2024-02-26 23:58:22 +0530

    5Nov's Solved JS Assignment added

---
+++ Javascript Solved Assignments/2_Assignment_5Nov'23 Javascript Fundamental/1.js
@@ -0,0 +1,9 @@
+/*
+Usage of Ternary Operator
+->  A ternary operator is a shorthand way of writing an if-else statement. It allows a programmer to execute one block of code if a particular condition is true, and another block of code if the condition is false, all within a single line of code.
+
+Syntax: `result = condition ? true expression : false expression;`;  
+*/
+
+let Num = 5;
+console.log(Num%2 == 0 ? 'Num is even':'Num is odd');
---
+++ Javascript Solved Assignments/2_Assignment_5Nov'23 Javascript Fundamental/3_NestedTernary.js
@@ -0,0 +1,3 @@
+let number = 21;
+let result = number > 0 ? "positive" : number < 0 ? "Negative" : "Zero";
+console.log(result);
